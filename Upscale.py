23a70ea5e203cd3760453084eb423913e473651d
Joseph Hodes <jojoho70@gmail.com>
2021-05-10 20:43:42 -0400

    refractor

--- Upscale.py
+++ Upscale.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-import glob 
-import os 
-from PIL import Image, ImageEnhance,ImageFilter
+import glob
+import os
+from PIL import Image, ImageEnhance, ImageFilter
 import operator
 from imutils import paths
 import argparse
@@ -11,53 +11,57 @@ import mahotas as mh
 import colorcorrect
 import colorcorrect.algorithm as cca
 from colorcorrect.util import from_pil, to_pil
-path="/Users/joseph/Desktop/Stitch/Newdata/"
+
+# set your path.
+path = "/Users/joseph/Desktop/Stitch/Newdata/"
 
 
 def ConvertOpencvToPill(image):
     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-    return  img
+    return img
+
 
 def variance_of_laplacian(image):
-	# compute the Laplacian of the image and then return the focus
-	# measure, which is simply the variance of the Laplacian
-	return cv2.Laplacian(image, cv2.CV_64F).var()
+    # compute the Laplacian of the image and then return the focus
+    # measure, which is simply the variance of the Laplacian
+    # this will be used in a future version
+    return cv2.Laplacian(image, cv2.CV_64F).var()
 
-def ConvertpiltoOpencv(image):
-    firstimage=image.convert('RGB')
+
+def pilToOpencv(image):
+    firstimage = image.convert('RGB')
     open_cv_image = np.array(firstimage)
-    open_cv_image = open_cv_image[:, :, ::-1].copy() 
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
     return open_cv_image
-def denoiser(image):
-    denoise = cv2.fastNlMeansDenoising(image, None, 15, 15)
-    return denoise
-def firstdenoiser(image):
 
-    sharpen_kernel = np.array([[-.4,-.4,-.4], [-.4,5,-.4], [-.4,-.4,-.4]])
+
+def denoiser(image):
+
+    sharpen_kernel = np.array(
+        [[-.4, -.4, -.4], [-.4, 5, -.4], [-.4, -.4, -.4]])
     sharpen = cv2.filter2D(image, -1, sharpen_kernel)
+    sharpen = antialiasing(sharpen)
     # converted_img = cv2.cvtColor(sharpen , cv2.COLOR_GRAY2BGR)
     denoise = cv2.fastNlMeansDenoising(sharpen, None, 20, 20)
-    cv2.imshow("denoise",denoise)
+    cv2.imshow("denoise", denoise)
     return denoise
+
+
 def antialiasing(image):
-    sharpen_kernel = np.array([[-1.13,-1.13,-1.13], [-1.13,10,-1.13], [-1.13,-1.13,-1.13]])
+    sharpen_kernel = np.array(
+        [[-1.13, -1.13, -1.13], [-1.13, 10, -1.13], [-1.13, -1.13, -1.13]])
     sharpen = cv2.filter2D(image, -1, sharpen_kernel)
     return sharpen
-def denoiserz(image):
-    denoise = cv2.fastNlMeansDenoising(image, None, 10, 10)
-    return denoise
+
 
 files = glob.glob("*png")
-startnum=1
+startNum = 1
 for i in files:
     print("Current file is: "+i)
-    print(files)
-    result=Image.open(i)
-
-    x= ConvertpiltoOpencv(result)
-    x=firstdenoiser(x)
-    x=ConvertOpencvToPill(x)
-    cv2.imwrite(str(startnum)+".png",x)
+    result = Image.open(i)
+    img = pilToOpencv(result)
+    img = denoiser(img)
+    img = ConvertOpencvToPill(img)
+    cv2.imwrite(str(startNum)+".png", img)
     print(i+" was sucessefully upscaled!")
-    startnum+=1
+    startNum += 1
--- imageupscaler.py
+++ imageupscaler.py
@@ -1,43 +1,45 @@
-import cv2;
+import cv2
 import numpy as np
-import glob 
-import os 
+import glob
+import os
 from PIL import Image
 import operator
 from imutils import paths
 import argparse
 import imageio
 import math
-length=1768
-width=2048
+length = 1768
+width = 2048
 
 
 def variance_of_laplacian(image):
-	# compute the Laplacian of the image and then return the focus
-	# measure, which is simply the variance of the Laplacian
-	return cv2.Laplacian(image, cv2.CV_64F).var()
+    # compute the Laplacian of the image and then return the focus
+    # measure, which is simply the variance of the Laplacian
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
 
 def ConvertpiltoOpencv(image):
-    firstimage=image.convert('RGB')
+    firstimage = image.convert('RGB')
     open_cv_image = np.array(firstimage)
-    open_cv_image = open_cv_image[:, :, ::-1].copy() 
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
     return open_cv_image
+
 
 def denoiser(image):
 
-    sharpen_kernel = np.array([[-.4,-.4,-.4], [-.4,5,-.4], [-.4,-.4,-.4]])
+    sharpen_kernel = np.array(
+        [[-.4, -.4, -.4], [-.4, 5, -.4], [-.4, -.4, -.4]])
     sharpen = cv2.filter2D(image, -1, sharpen_kernel)
     # converted_img = cv2.cvtColor(sharpen , cv2.COLOR_GRAY2BGR)
     denoise = cv2.fastNlMeansDenoising(sharpen, None, 20, 20)
-    cv2.imshow("denoise",denoise)
+    cv2.imshow("denoise", denoise)
     return denoise
+
 
 def ConvertOpencvToPill(image):
     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-    return  img
 
-
+    return img
 
 
 path = glob.glob("*png")
@@ -45,71 +47,67 @@ cv_img = []
 for img in path:
     l = cv2.imread(img)
     # maner = l.size
-    length+=2048
-    width+=1768
+    length += 2048
+    width += 1768
     print(length)
     print(width)
     n = cv2.imread(img)
     cv_img.append(n)
-path=np.sort(path)
-rowvalues= round(math.sqrt(len (cv_img)))
+path = np.sort(path)
+rowvalues = round(math.sqrt(len(cv_img)))
 print(path)
 # only overlay on the x on beginning layer
 # then x and y above next layer
 
-#calc all the sizes of image or dataset
+# calc all the sizes of image or dataset
 # suppose img2 is to be shifted by `shift` amount
-overlay=0.1
-offsetx=width-round(width*overlay)
-offsety=length-round((length*2)*length) 
+overlay = 0.1
+offsetx = width-round(width*overlay)
+offsety = length-round((length*2)*length)
 shift = (0, 0)
-y=0
-x=0
+y = 0
+x = 0
 # compute the size of the panorama
 shift = (offsetx, 0)
 # paste img1 on top of img2
-finaleimage=Image.new('RGBA',size=(length,width),color=(0,0,0,0))
+finaleimage = Image.new('RGBA', size=(length, width), color=(0, 0, 0, 0))
 for j in range(rowvalues):
     for i in range(rowvalues):
-        if(i=0)
+        if i == 0:
             newimg1 = Image.new('RGBA', size=(1768, 2048), color=(0, 0, 0, 0))
-            firstimage=Image.open(path[0])
+            firstimage = Image.open(path[0])
             firstimage.show("image num:"+str(0))
-            newimg1.paste(firstimage,(x, y))
-            img1=Image.open(path[i+1])
-            img1=ConvertpiltoOpencv(img1)
-            img1= denoiser(img1)
-            img1=ConvertOpencvToPill(img1)
-            img=Image.fromarray(img1)
+            newimg1.paste(firstimage, (x, y))
+            img1 = Image.open(path[i+1])
+            img1 = ConvertpiltoOpencv(img1)
+            img1 = denoiser(img1)
+            img1 = ConvertOpencvToPill(img1)
+            img = Image.fromarray(img1)
             newimg1.paste(img, (offsetx, offsety))
 
             newimg2 = Image.new('RGBA', size=(1768, 2048), color=(0, 0, 0, 0))
-            firstimage=Image.open(path[i])
-            firstimage= ConvertpiltoOpencv(firstimage)
-            firstimage= denoiser(firstimage)
-            firstimage= ConvertOpencvToPill(firstimage)
-            firstimage= Image.fromarray(firstimage)
-            newimg1.paste(firstimage,(x, y))
+            firstimage = Image.open(path[i])
+            firstimage = ConvertpiltoOpencv(firstimage)
+            firstimage = denoiser(firstimage)
+            firstimage = ConvertOpencvToPill(firstimage)
+            firstimage = Image.fromarray(firstimage)
+            newimg1.paste(firstimage, (x, y))
             firstimage.show("image num:"+str(i))
-            img1=  Image.open(path[i+1])
-            img1= ConvertpiltoOpencv(img1)
-            img1= denoiser(img1)
-            img1=ConvertOpencvToPill(img1)
-            img1=
-            newimg1.paste(img, (offsetx, offsety))
-            x+=offsetx
-            # blend with alpha=0.4
+            img1 = Image.open(path[i+1])
+            img1 = ConvertpiltoOpencv(img1)
+            img1 = denoiser(img1)
+            img1 = ConvertOpencvToPill(img1)
 
-            finaleimage.paste(result,(x,y))
+            newimg1.paste(img, (offsetx, offsety))
+            x += offsetx
+            # blend with alpha=0.4
             firstimage.show("image num:"+str(i))
-            offsetx+=offsetx
-    y+=offsety
+            offsetx += offsetx
+    y += offsety
 finaleimage.show("man")
 
 finaleimage.save("oof.png")
-hecc=ConvertpiltoOpencv(result)
-blurRating=variance_of_laplacian(hecc)
-print(blurRating)
+
 
 # grabimage=Image.open("000000000.png")
 
@@ -128,8 +126,7 @@ print(blurRating)
 # cv2.imshow('full image', sharpen)
 # cv2.imshow("first",img)
 cv2.waitKey(0)
- 
-# cv2.waitKey(2000)
- 
-cv2.destroyAllWindows()
 
+# cv2.waitKey(2000)
+
+cv2.destroyAllWindows()
